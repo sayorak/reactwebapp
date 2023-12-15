a5879ea46b70cefb15a172327f94743a304eebb1
sayora <sayora@gmail.com>
2023-12-15 07:22:51 +0600

    7

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Page loaded');
 
-    // ... (previous code)
+    const subscribeButton = document.querySelector('button[type="submit"]');
+    subscribeButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        const emailInput = document.getElementById('email');
+        const subscribeCheckbox = document.getElementById('subscribe');
 
-    // Mouseover and mouseout event listeners for the menu dropdown
+        alert(`Subscribed: ${emailInput.value}, ${subscribeCheckbox.checked ? 'Yes' : 'No'}`);
+    });
+
+    const tableRows = document.querySelectorAll('table tbody tr');
+    tableRows.forEach(row => {
+        row.addEventListener('click', function () {
+            alert(`Clicked on event: ${row.children[1].textContent}`);
+        });
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            alert('Escape key pressed!');
+        }
+    });
+
+    const menuButton = document.getElementById('menu-button');
+    const menuDropdown = document.getElementById('menu-dropdown');
+
+    menuButton.addEventListener('click', function () {
+        menuDropdown.classList.toggle('show');
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            menuDropdown.classList.remove('show');
+        }
+    });
     menuButton.addEventListener('mouseover', function () {
         menuDropdown.classList.add('show');
     });
 
     menuDropdown.addEventListener('mouseout', function (event) {
-        // Check if the mouse is not over the menu button or the dropdown
         if (!event.relatedTarget || (event.relatedTarget !== menuButton && !menuButton.contains(event.relatedTarget) && !menuDropdown.contains(event.relatedTarget))) {
             menuDropdown.classList.remove('show');
         }
     });
 
-    // Additional mouse events for table rows
     tableRows.forEach(row => {
         row.addEventListener('mouseover', function () {
             row.style.backgroundColor = '#e6f7ff';
@@ -26,9 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Click event listener for the entire document
     document.addEventListener('click', function (event) {
-        // Check if the click is outside the menu dropdown
         if (!menuDropdown.contains(event.target) && !menuButton.contains(event.target)) {
             menuDropdown.classList.remove('show');
         }
